490e79571caf4c67cd385df8fb7937e4ff69265b
Irving Didier <bider88@gmail.com>
2018-06-12 14:42:58 -0500

    Agregando archivos estáticos, bootstrap y mi propio estilo

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    const salida = {
-        nombre: 'Irving Didier',
-        edad: 29,
-        url: req.url
-    }
-    res.send(salida)
-})
+// Settings
 
+// Middlewares
+app.use(express.static(__dirname + '/public'))
+
+// Routes
+// app.get('/', (req, res) => {
+//     const salida = {
+//         nombre: 'Irving Didier',
+//         edad: 29,
+//         url: req.url
+//     }
+//     res.send(salida)
+// })
+
+// starting the server
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000')
 })
